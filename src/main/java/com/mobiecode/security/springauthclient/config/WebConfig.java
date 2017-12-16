9967ef69e7ee5c5a7194d769afe006c6e29ada3a
Socheat <soc.chea@gl-f.com>
2017-12-16 11:43:53 +0700

    Auth Client

---
+++ src/main/java/com/mobiecode/security/springauthclient/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.mobiecode.security.springauthclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable(); //Enable Servlet Configuration
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/index");
+
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
